40a38fd4c90e2e9b7803cea1e0b1c314dfde213b
sadman <sadmanhossain400@gmail.com>
2023-08-04 23:59:56 +0600

    frontend commit part one

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,41 +1,41 @@
-import './footer.scss'
+import "./footer.scss";
 
 const Footer = () => {
   return (
-
     <>
-    <div className='container Footer'>
+      <div className="container Footer">
         <div className="footer_wrapper">
-            <div className="footer_info">
-                <img src="./images/logoW.png" alt="" />
-                <p>© 2023 SmartCitiesWorld</p>
-                <p>Smart Cities World Ltd, Room 2.13, 27 Clements Lane, London, EC4N 7AE</p>
+          <div className="footer_info">
+            <img src="./images/logoW.png" alt="" />
+            <p>© 2023 Smartcitycitizen </p>
+            <p>
+              Smart City Citizen, Bconz International Pvt. Ltd, Bangalore, India
+            </p>
+          </div>
+          <div className="footer_nav">
+            <div>
+              <a href="/about-us">About Us</a>
+              <a href="/partner-withus">Partner with us</a>
+              <a href="/contact-us">Contact us</a>
+              <a href="/smartcity-projectreports">Smart City Project Reports</a>
             </div>
-            <div className="footer_nav">
-                <div>
-                    <a href="/about-us">About Us</a>
-                    <a href="/the-team">The Team</a>
-                    <a href="/contact-us">Contact us</a>
-                    <a href="/special-reports">Special Reports</a>
-                </div>
-                <div>
-                    <a href="/webinars">Webinars</a>
-                    <a href="/white-papers">White paper</a>
-                    <a href="/trend-reports">Trend Reports</a>
-                    <a href="/podcasts">Podcasts</a>
-                </div>
-                <div>
-                    <a href="/city-profile">City Profile</a>
-                    <a href="/city-lights">City lights</a>
-                    <a href="/editors-blog">Editors blog</a>
-                    <a href="/opinions">Opinions</a>
-                </div>
+            <div>
+              <a href="/jobs">Jobs</a>
+              <a href="/parners">Partners</a>
+              <a href="/citizen-engagement">Citizen Engagement</a>
+              <a href="/support">Support</a>
             </div>
+            <div>
+              <a href="/city-profile">City Profile</a>
+              <a href="/city-lights">City lights</a>
+              <a href="/editors-blog">Blog</a>
+              <a href="/opinions">Opinions</a>
+            </div>
+          </div>
         </div>
-    </div>
-    
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
