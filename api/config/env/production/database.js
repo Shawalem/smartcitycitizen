e02563374748c5e4d73b137eb692b6e4b96d3266
sadman <sadmanhossain400@gmail.com>
2023-08-09 20:16:25 +0600

    strapi adding code to database

--- api/config/env/production/database.js
+++ api/config/env/production/database.js
@@ -1,17 +1,23 @@
 
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
+
 module.exports = ({ env }) => ({
+  connection: {  
+    client: 'postgres',
     connection: {
-      client: 'postgres', 
-    connection: {
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapidb'),
-          user: env('DATABASE_USERNAME', ''),
-          password: env('DATABASE_PASSWORD', ''),
-          ssl: env.bool("DATABASE_SSL", false) && {
-            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-        },
-        },
-        debug: false,
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-  });
+      debug: false,
+  },
+});
