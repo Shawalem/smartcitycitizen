6e1f9ab01207db43eead6a4750ca758d12d46774
sadman <sadmanhossain400@gmail.com>
2023-08-16 19:04:26 +0600

    adding console log in ad pages

--- frontend/src/components/adSec/Ad.jsx
+++ frontend/src/components/adSec/Ad.jsx
@@ -4,6 +4,7 @@ import useFetch from '../../hooks/useFetch'
 const Ad = ({adpanels}) => {
 
   const adImg = adpanels?.attributes?.img?.data?.attributes?.url;
+  console.log(adImg)
   
   return (
     <div className='container'>
