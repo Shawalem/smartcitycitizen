687efe130d6421deab716ff52ee1c7494975b099
sadman <sadmanhossain400@gmail.com>
2023-08-25 00:48:35 +0600

    update\

---
+++ api/src/api/test/controllers/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cityresource controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::test.test');
