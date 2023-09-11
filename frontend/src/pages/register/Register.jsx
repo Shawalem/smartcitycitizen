b631d1e5d317e27779cf8dd0bec10a65a8bfa4dc
sadman <sadmanhossain400@gmail.com>
2023-09-12 02:43:49 +0600

    update

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -9,7 +9,7 @@ const Register = () => {
   const { handleSubmit, register, formState: { errors } } = useForm();
 
   const onSubmit = (data)=>{
-      axios.post(`${import.meta.env.VITE_REACT_APP_API_URL}/auth/local`,data)
+      axios.post(`${import.meta.env.VITE_REACT_APP_API_URL}/auth/local/register`,data)
       .then(res =>{
         console.log(res.data);
       }).catch(e=>{
