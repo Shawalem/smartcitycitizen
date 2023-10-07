483c62a911c003d659bd27e6e22bd8dcbc301628
sadman <sadmanhossain400@gmail.com>
2023-10-07 16:52:53 +0600

    update

--- frontend/src/components/adSec/Ad.jsx
+++ frontend/src/components/adSec/Ad.jsx
@@ -4,20 +4,20 @@ import useFetch from '../../hooks/useFetch'
 const Ad = () => {
 
   const { data: adpanels } = useFetch("/adpanels?populate=*")
+  console.log(adpanels);
 
   if (!adpanels) {
     return null; // or you can return a placeholder component or message
   }
 
-  const adImg = adpanels?.img?.data?.attributes?.url;
+  const adImg = adpanels?.[0]?.attributes?.img?.data?.attributes?.url;
   console.log(adImg)
   
   return (
     <div className='container'>
         <div className="ad">
           <div className="ad_img">
-          {/* <img src={`https://backend-app-lft6m.ondigitalocean.app${adImg}`} alt="ad" /> */}
-          <img src={`./images/headerAd.gif`} alt="ad" />
+          <img src={`${adImg}`} alt="ad" />
           </div>
         </div>
     </div>
