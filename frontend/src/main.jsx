2fb6cafcdced1dd064c483cd0824c47b0cb93ffe
Tanvir1909R <trcr719@gmail.com>
2023-10-11 20:32:46 +0600

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,28 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./contexts/UserContext.jsx";
+import { hydrate, render } from "react-dom";
 import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <UserContext>
+//       <App />
+//     </UserContext>
+//   </BrowserRouter>
+// );
+
+const Main = () => (
   <BrowserRouter>
     <UserContext>
       <App />
     </UserContext>
   </BrowserRouter>
 );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<Main />, rootElement);
+} else {
+  render(<Main />, rootElement);
+}
