db70007a372f4403074dfeb5ebf9dbe22568f1e9
Tanvir1909R <trcr719@gmail.com>
2023-10-12 19:20:35 +0600

    udpate

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./contexts/UserContext.jsx";
 import { hydrate, render } from "react-dom";
 import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <UserContext>
-      <App />
-    </UserContext>
-  </BrowserRouter>
-);
-
-// const Main = () => (
+// ReactDOM.createRoot(document.getElementById("root")).render(
 //   <BrowserRouter>
 //     <UserContext>
 //       <App />
@@ -22,9 +14,22 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 //   </BrowserRouter>
 // );
 
-// const rootElement = document.getElementById("root");
-// if (rootElement.hasChildNodes()) {
-//   hydrate(<Main />, rootElement);
-// } else {
-//   render(<Main />, rootElement);
-// }
+const Main = () => (
+  <BrowserRouter>
+    <UserContext>
+      <App />
+    </UserContext>
+  </BrowserRouter>
+);
+
+const root = createRoot(document.getElementById("root"));
+
+const isServerRendered = root._internalRoot !== null;
+
+if (isServerRendered) {
+  // If it's server-rendered, use render
+  root.render(<Main />);
+} else {
+  // Otherwise, use hydrate
+  root.hydrate(<Main />);
+}
